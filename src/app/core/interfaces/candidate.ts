32b31575f64636fcaf625e2596ac9c87bca5d068
David Uczen <david.j.uczen@gmail.com>
2023-02-09 22:13:21 -0500

    Locla changes

--- src/app/core/interfaces/candidate.ts
+++ src/app/core/interfaces/candidate.ts
@@ -1,6 +1,16 @@
+import { PhysicalExam } from "./physical-exam";
+
 export interface Candidate {
     id: number;
     cycleId: number;
+    cycleCont: number;
+    audit: boolean;
+    poom: boolean;
+    essays: number;
+    letters: number;
+    preExamWritten: number;
+    examWritten: number;
+    physicalExam: PhysicalExam;
     userId: string;
     displayName: string;
 }
---
+++ src/app/core/interfaces/physical-exam.ts
@@ -0,0 +1,11 @@
+export interface PhysicalExam {
+  preExamRun: number;
+  preExamPushUps: number;
+  preExamSitUps: number;
+  preExamBurpees: number;
+
+  examRun: number;
+  examPushUps: number;
+  examSitUps: number;
+  examBurpees: number;
+}
